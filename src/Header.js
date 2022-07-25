4b5f0c0f5ff49277fc754a8483f43ee5e873f6bc
pdxgrantc <pdxgrantc@gmail.com>
2022-07-25 14:06:12 -0700

    header edits

--- src/Header.js
+++ src/Header.js
@@ -8,8 +8,8 @@ export default function Header() {
                     <h1 class="px-[3vw] leading-[4.75rem] h-fit bg-button_color hover:bg-button_pressed_color hover:cursor-pointer">To-Do</h1>
                 </div>
                 <div class="my-auto h-fit flex text-[2.5rem] font-semibold mobile:text-4xl">
-                    <h2 class="px-[1.5vw] py-[.25vh] leading-[3.75rem] bg-button_color hover:bg-button_pressed_color hover:cursor-pointer">Home</h2>
-                    <h2 class="px-[1.5vw] py-[.25vh] leading-[3.75rem] bg-button_color hover:bg-button_pressed_color hover:cursor-pointer">Add To-Do</h2>
+                    <h2 class="px-[1vw] py-[.25vh] leading-[3.75rem] bg-button_color hover:bg-button_pressed_color hover:cursor-pointer">Home</h2>
+                    <h2 class="px-[1vw] py-[.25vh] leading-[3.75rem] bg-button_color hover:bg-button_pressed_color hover:cursor-pointer">Add To-Do</h2>
                 </div>
             </div>
             <div class="mb-[3.5vh]"></div>
