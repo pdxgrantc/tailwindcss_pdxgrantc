e413a3ac74b8ca01fbbf7bc7ac33ce69c8a2f094
pdxgrantc <pdxgrantc@gmail.com>
2022-07-27 07:16:41 -0700

    new item nav bar

--- src/NewItem/NewItem.js
+++ src/NewItem/NewItem.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import Header from "../partials/Header"
 
 export default function NewItem() {
   return (
-    <div>NewItem</div>
+    <div class="w-[75%] m-auto bg-main_bg_color text-text_white">
+      <Header />
+    </div>
+    
   )
 }
--- src/partials/Header.js
+++ src/partials/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
         <>
             <div class="flex justify-between mt-[3.54vh]">
                 <div class="text-[2.75rem] h-full font-bold mobile:text-4xl">
-                    <h1 class="px-[3vw] leading-[4.75rem] h-fit bg-button_color hover:bg-button_pressed_color hover:cursor-pointer">To-Do</h1>
+                    <Link class="py-[1vh] px-[3vw] leading-[4.75rem] h-fit bg-button_color hover:bg-button_pressed_color hover:cursor-pointer" to="/">To-Do</Link>
                 </div>
                 <div class="my-auto h-fit flex text-[2.5rem] font-semibold mobile:text-4xl">
                     <Link class="px-[1vw] py-[.25vh] leading-[3.75rem] bg-button_color hover:bg-button_pressed_color hover:cursor-pointer" to="/">Home</Link>
