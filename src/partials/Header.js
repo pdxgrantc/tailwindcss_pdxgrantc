841bd5aeccf147d51faaefca61243da97bdbf5a9
pdxgrantc <pdxgrantc@gmail.com>
2022-08-08 13:03:45 -0700

    organization

--- src/partials/Header.js
+++ src/partials/Header.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Header() {
     return (
         <div class="flex justify-between ">
-            <Link class="w-[35vw] bg-black pl-[5vw] text-[3.75rem] font-bold leading-[5.25rem] h-[12vh] hover:cursor-pointer mobile:text-4xl" to="/">Grant Conklin</Link>
+            <Link class="w-[35vw] bg-black pl-[5vw] text-[3.75rem] font-bold leading-[5.75rem] h-[12vh] hover:cursor-pointer mobile:text-4xl" to="/">Grant Conklin</Link>
             <div class="pr-[5vw] flex text-[2.75rem] font-semibold mobile:text-4xl">
                 <Link class="h-fit align-middle px-[1vw] py-[.5vw] hover:bg-button_pressed_color hover:cursor-pointer hover:ease-in-out duration-[150ms]" to="/">Code</Link>
                 <Link class="h-fit align-middle px-[1vw] py-[.5vw] hover:bg-button_pressed_color hover:cursor-pointer hover:ease-in-out duration-[150ms]" to="/ToDo">Apps</Link>
