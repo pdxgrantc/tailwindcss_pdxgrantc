70a4848eda00942f5989c4a6606264d7a4c064be
pdxgrantc <pdxgrantc@gmail.com>
2022-08-08 22:59:42 -0700

    Variable header links

--- src/Pages/AboutThisSite/AboutThisSite.js
+++ src/Pages/AboutThisSite/AboutThisSite.js
@@ -3,10 +3,30 @@ import React from "react";
 import Header from "../../Static/partials/HomeHeader";
 import Footer from "../../Static/partials/Footer";
 
+import react from "../../Static/Images/react.png";
+import HomeScreeenshot from "../../Static/Images/HomeScreenshot.jpg";
+
 export default function AboutThisSite() {
     return (
         <div class="m-auto bg-main_bg_color text-text_white">
-            <Header />
+            <Header title={"About This Site"} />
+            <div class="h-[6vh]"></div>
+            <div class="mx-[7vw]">
+                <div class="pr-[5%] w-[100%] flex justify-between bg-black ">
+                    <img class="w-[58%] h-auto" src={HomeScreeenshot} alt="HomePage"></img>
+                    <div class="w-[42%] flex flex-col justify-center gap-5 pb-2 px-[3vw]">
+                        <h2 class="text-6xl text-text_pink">Origins</h2>
+                        <h3 class="align-middle leading-[1.1] text-[2.5rem] text-wrap">I developed this site as a display of ability in full stack development. As well as to have a place to host my self developed web apps.</h3>
+                    </div>
+                </div>
+                <div class="h-[6vh]"></div>
+                <div class="flex justify-between">
+                    <div class="bg-black w-full">
+                    </div>
+                    <img class="h-auto w-[30vw]" src={react} alt="React"></img>
+                </div>
+            </div>
+            <div class="h-[6vh]"></div>
             <Footer />
         </div>
     );
--- src/Pages/ToDo/partials/ToDoHeader.js
+++ src/Pages/ToDo/partials/ToDoHeader.js
@@ -1,31 +1,13 @@
 import React from 'react'
-import { Link } from "react-router-dom";
-/*
-export default function ToDoHeader() {
-    return (
-        <>
-            <div class="flex justify-between mt-[3.54vh]">
-                <div class="text-[2.75rem] h-full font-bold mobile:text-4xl">
-                    <Link class="py-[1vh] px-[3vw] leading-[4.75rem] h-fit bg-button_color hover:bg-button_pressed_color hover:cursor-pointer" to="/">PDX</Link>
-                </div>
-                <div class="my-auto h-fit flex text-[2.5rem] font-semibold mobile:text-4xl">
-                    <Link class="px-[1vw] py-[.25vh] leading-[3.75rem] bg-button_color hover:bg-button_pressed_color hover:cursor-pointer" to="/ToDo">Home</Link>
-                    <Link class="px-[1vw] py-[.25vh] leading-[3.75rem] bg-button_color hover:bg-button_pressed_color hover:cursor-pointer" to="/ToDo/new-item">New Item</Link>
-                </div>
-            </div>
-            <div class="mb-[3.5vh]"></div>
-        </>
-    )
-}
-*/
+import { Link } from "react-router-dom"
 
 export default function ToDoHeader() {
     return (
         <div class="flex justify-between ">
             <Link class="w-[35vw] bg-black pl-[5vw] text-[3.75rem] font-bold leading-[5.75rem] h-[12vh] hover:cursor-pointer mobile:text-4xl" to="/ToDo">ToDo List</Link>
             <div class="pr-[5vw] flex text-[2.75rem] font-semibold mobile:text-4xl">
-                <Link class="h-fit align-middle px-[1vw] py-[.5vw] hover:bg-button_pressed_color hover:cursor-pointer hover:ease-in-out duration-[150ms]" to="/">Home</Link>
-                <Link class="h-fit align-middle px-[1vw] py-[.5vw] hover:bg-button_pressed_color hover:cursor-pointer hover:ease-in-out duration-[150ms]" to="/ToDo">Apps</Link>
+                <Link class="h-fit align-middle px-[1vw] py-[.5vw] hover:bg-button_pressed_color hover:cursor-pointer hover:ease-in-out duration-[350ms]" to="/">Home</Link>
+                <Link class="h-fit align-middle px-[1vw] py-[.5vw] hover:bg-button_pressed_color hover:cursor-pointer hover:ease-in-out duration-[350ms]" to="/ToDo">Apps</Link>
                 <Link class="h-fit align-middle px-[1vw] py-[.5vw] bg-button_accent_color hover:bg-transparent hover:cursor-pointer hover:ease-in-out duration-[350ms]" to="/ToDo/New-Item">New Item</Link>
             </div>
         </div>
