a5f50daf71c0985b9828818eaf058e9bb18b18eb
pdxgrantc <pdxgrantc@gmail.com>
2022-08-17 13:00:04 -0700

    text size mobile home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -15,10 +15,10 @@ export default function Home() {
                 <Header />
                 <div class="relative on_mobile:flex on_mobile:flex-col on_mobile:w-[90vw] on_mobile:m-auto">
                     <img class="max-w-[50vw] h-[88vh] w-auto on_mobile:w-[100%] on_mobile:max-w-none on_mobile:h-auto" src={Grant_Photo} alt="Grant"></img>
-                    <div class="max-w-[43vw] pl-[2vw] absolute right-[5vw] bottom-[40vh] on_mobile:max-w-none on_mobile:p-0 on_mobile:static on_mobile:w-[100%]">
-                        <h2 class="text-[4.5rem] font-bold mobile:text-4xl">I'm Grant Conklin</h2>
-                        <h3 class="text-[3.5rem] font-semibold mobile:text-4xl ">Full Stack Web Developer</h3>
-                        <h3 class="text-[3.5rem] font-semibold mobile:text-4xl text-text_grey">Student at Oregon State University</h3>
+                    <div class="max-w-[43vw] pl-[2vw] absolute right-[5vw] bottom-[40vh] on_mobile:max-w-none on_mobile:p-0 on_mobile:static on_mobile:w-[100%] on_mobile:mt-3">
+                        <h2 class="text-[4.5rem] font-bold on_mobile:text-[2.75rem]">I'm Grant Conklin</h2>
+                        <h3 class="text-[3.5rem] font-semibold mobile:text-4xl on_mobile:text-[2rem]">Full Stack Web Developer</h3>
+                        <h3 class="text-[3.5rem] font-semibold mobile:text-4xl text-text_grey on_mobile:text-[2rem]">Student at Oregon State University</h3>
                     </div>
                 </div>
                 <div class="w-[35vw] h-[5vh] bg-black on_mobile:bg-transparent"></div>
