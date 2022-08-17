a7e28d178ed0910d8e670da9065fdb30522770b4
pdxgrantc <pdxgrantc@gmail.com>
2022-08-17 12:54:34 -0700

    photo and home titles for mobile

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -13,15 +13,15 @@ export default function Home() {
         <>
             <div class="m-auto bg-main_bg_color text-text_white">
                 <Header />
-                <div class="relative hidden">
-                    <img class="max-w-[50vw] h-[88vh] w-auto" src={Grant_Photo} alt="Grant"></img>
-                    <div class="max-w-[43vw] pl-[2vw] absolute right-[5vw] bottom-[40vh]">
+                <div class="relative on_mobile:flex on_mobile:flex-col on_mobile:w-[90vw] on_mobile:m-auto">
+                    <img class="max-w-[50vw] h-[88vh] w-auto on_mobile:w-[100%] on_mobile:max-w-none on_mobile:h-auto" src={Grant_Photo} alt="Grant"></img>
+                    <div class="max-w-[43vw] pl-[2vw] absolute right-[5vw] bottom-[40vh] on_mobile:max-w-none on_mobile:p-0 on_mobile:static on_mobile:w-[100%]">
                         <h2 class="text-[4.5rem] font-bold mobile:text-4xl">I'm Grant Conklin</h2>
                         <h3 class="text-[3.5rem] font-semibold mobile:text-4xl ">Full Stack Web Developer</h3>
                         <h3 class="text-[3.5rem] font-semibold mobile:text-4xl text-text_grey">Student at Oregon State University</h3>
                     </div>
                 </div>
-                <div class="w-[35vw] h-[5vh] bg-black on_mobile:hidden"></div>
+                <div class="w-[35vw] h-[5vh] bg-black on_mobile:bg-transparent"></div>
                 <Resume />
                 <div class="mb-[8vh]"></div>
                 <Footer />
