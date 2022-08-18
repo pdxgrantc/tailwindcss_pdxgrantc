13315a35cb689cd8943c50499e44e046817aa9e5
pdxgrantc <pdxgrantc@gmail.com>
2022-08-18 16:37:07 -0700

    mobile header size

--- src/Static/partials/DeviceHeaders/MobileHeader.js
+++ src/Static/partials/DeviceHeaders/MobileHeader.js
@@ -4,8 +4,7 @@ import { Link } from "react-router-dom";
 export default function MobileHeader() {
     if (window.location.pathname === "/") {
         return (
-            <div class="on_desktop:hidden flex justify-around bg-black w-[90vw] m-auto h-[5vh] text-[1.25rem]">
-                <Link class="h-fit my-auto" to="/">Home</Link>
+            <div class="on_desktop:hidden flex justify-around bg-black w-[90vw] m-auto h-[10vh] text-[1.85rem]">
                 <Link class="h-fit my-auto" to="/Code">Code</Link>
                 <Link class="h-fit my-auto" to="/ToDo">Apps</Link>
                 <Link class="h-fit my-auto" to="/about-this-site">About This Site</Link>
