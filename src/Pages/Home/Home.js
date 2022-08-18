cfa6168cc7da7272ca4e3fd2bc0c42d6678f4bab
pdxgrantc <pdxgrantc@gmail.com>
2022-08-18 11:07:21 -0700

    mobile height

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -13,7 +13,7 @@ export default function Home() {
         <>
             <div class="m-auto bg-main_bg_color text-text_white">
                 <Header />
-                <div class="relative on_mobile:mt-[2vh] on_mobile:flex on_mobile:gap-[5vh] on_mobile:flex-col on_mobile:w-[90vw] on_mobile:m-auto on_mobile:h-[95vh]">
+                <div class="relative on_mobile:mt-[2vh] on_mobile:flex on_mobile:gap-[5vh] on_mobile:flex-col on_mobile:w-[90vw] on_mobile:m-auto on_mobile:h-[93vh]">
                     <img class="max-w-[50vw] h-[88vh] w-auto on_mobile:w-[100%] on_mobile:max-w-none on_mobile:h-auto" src={Grant_Photo} alt="Grant"></img>
                     <div class="max-w-[43vw] pl-[2vw] absolute right-[5vw] bottom-[40vh] on_mobile on_mobile:max-w-none on_mobile:p-0 on_mobile:static on_mobile:w-[100%]">
                         <h2 class="text-[4.5rem] font-bold on_mobile:text-[2.75rem]">I'm Grant Conklin</h2>
@@ -21,7 +21,7 @@ export default function Home() {
                         <h3 class="text-[3.5rem] font-semibold text-text_grey on_mobile:text-[2rem]">Student at Oregon State University</h3>
                     </div>
                 </div>
-                <div class="w-[35vw] h-[5vh] bg-black on_mobile:hidden"></div>
+                <div class="w-[35vw] h-[5vh] bg-black on_mobile:h-0"></div>
                 <Resume />
                 <div class="mb-[8vh]"></div>
                 <Footer />
