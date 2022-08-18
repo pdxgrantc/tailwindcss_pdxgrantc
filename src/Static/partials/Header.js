8b26cd51c20ddba7a8b83f207155b93f42b1d0f6
pdxgrantc <pdxgrantc@gmail.com>
2022-08-18 09:00:45 -0700

    hide desktop header

--- src/Static/partials/Header.js
+++ src/Static/partials/Header.js
@@ -24,16 +24,8 @@ export default function Header({ title }) {
     return (
         <>
             <div class="on_mobile:hidden">
-                <div class="flex justify-between">
-                    <Link class="w-[35vw] bg-black pl-[5vw] text-[3.75rem] font-bold leading-[5.75rem] h-[12vh] hover:cursor-pointer mobile:text-4xl" to={title_link}>{title}</Link>
-                    <div class="pr-[5vw] flex text-[2.75rem] font-semibold mobile:text-4xl">
-                        <Link class="h-fit align-middle px-[1vw] py-[.5vw] hover:bg-button_pressed_color hover:cursor-pointer hover:ease-in-out duration-[350ms]" to="/Code">Code</Link>
-                        <Link class="h-fit align-middle px-[1vw] py-[.5vw] hover:bg-button_pressed_color hover:cursor-pointer hover:ease-in-out duration-[350ms]" to="/ToDo">Apps</Link>
-                        <Link class="h-fit align-middle px-[1vw] py-[.5vw] bg-button_accent_color hover:bg-transparent hover:cursor-pointer hover:ease-in-out duration-[350ms]" to={special_link}>{special_nav}</Link>
-                    </div>
-                </div>
-            </div>
                 <DesktopHeader />
+            </div>
             <div class="on_desktop:hidden flex justify-around bg-black w-[90vw] m-auto h-[5vh]">
                 <MobileHeader />
             </div>
