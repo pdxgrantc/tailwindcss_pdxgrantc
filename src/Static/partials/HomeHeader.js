44bd9f5b9253e6cb6f464c120561fb6f1d1a9a6e
pdxgrantc <pdxgrantc@gmail.com>
2022-08-18 08:47:16 -0700

    mobile header text size

--- src/Static/partials/HomeHeader.js
+++ src/Static/partials/HomeHeader.js
@@ -32,10 +32,10 @@ export default function HomeHeader({ title }) {
                 </div>
             </div>
             <div class="on_desktop:hidden flex justify-around bg-black w-[90vw] m-auto h-[5vh]">
-                <Link class="h-fit my-auto" to="/">Home</Link>
-                <Link class="h-fit my-auto" to="/Code">Code</Link>
-                <Link class="h-fit my-auto" to="/ToDo">Apps</Link>
-                <Link class="h-fit my-auto" to={special_link}>{special_nav}</Link>
+                <Link class="h-fit my-auto text-[1.5rem]" to="/">Home</Link>
+                <Link class="h-fit my-auto text-[1.5rem]" to="/Code">Code</Link>
+                <Link class="h-fit my-auto text-[1.5rem]" to="/ToDo">Apps</Link>
+                <Link class="h-fit my-auto text-[1.5rem]" to={special_link}>{special_nav}</Link>
             </div>
         </>
     )
