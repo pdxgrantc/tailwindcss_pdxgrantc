f7d1aaa1ae007fa206f2e90797319686fd0f9635
pdxgrantc <pdxgrantc@gmail.com>
2022-08-18 09:04:22 -0700

    title pass

--- src/Static/partials/Header.js
+++ src/Static/partials/Header.js
@@ -5,29 +5,14 @@ import DesktopHeader from './DesktopHeader';
 import MobileHeader from './MobileHeader';
 
 export default function Header({ title }) {
-    var special_nav = "About This Site"
-    var title_link = "/"
-    var special_link = "/About-This-Site"
-    if (title === undefined) {
-        title = "Grant Conklin"
-        title_link = "/"
-    }
-    else {
-        special_nav = "Home"
-    }
-
-    if (title === "About This Site") {
-        title_link = "/About-This-Site"
-        special_link = "/"
-    }
 
     return (
         <>
             <div class="on_mobile:hidden">
-                <DesktopHeader />
+                <DesktopHeader title/>
             </div>
             <div class="on_desktop:hidden flex justify-around bg-black w-[90vw] m-auto h-[5vh]">
-                <MobileHeader />
+                <MobileHeader title/>
             </div>
         </>
     )
