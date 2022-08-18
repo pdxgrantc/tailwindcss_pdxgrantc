3ab1ced974e4b89512d2efab42df397168915fc4
pdxgrantc <pdxgrantc@gmail.com>
2022-08-18 16:38:27 -0700

    text size

--- src/Static/partials/DeviceHeaders/MobileHeader.js
+++ src/Static/partials/DeviceHeaders/MobileHeader.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function MobileHeader() {
     if (window.location.pathname === "/") {
         return (
-            <div class="on_desktop:hidden flex justify-around bg-black w-[90vw] m-auto h-[10vh] text-[1.85rem]">
+            <div class="on_desktop:hidden flex justify-around bg-black w-[90vw] m-auto h-[10vh] text-[1.65rem]">
                 <Link class="h-fit my-auto" to="/Code">Code</Link>
                 <Link class="h-fit my-auto" to="/ToDo">Apps</Link>
                 <Link class="h-fit my-auto" to="/about-this-site">About This Site</Link>
@@ -13,7 +13,7 @@ export default function MobileHeader() {
     }
     else {
         return (
-            <div class="on_desktop:hidden flex justify-around bg-black w-[90vw] m-auto h-[5vh] text-[1.25rem]">
+            <div class="on_desktop:hidden flex justify-around bg-black w-[90vw] m-auto h-[5vh] text-[1.65rem]">
                 <Link class="h-fit my-auto" to="/">Home</Link>
                 <Link class="h-fit my-auto" to="/Code">Code</Link>
                 <Link class="h-fit my-auto" to="/ToDo">Apps</Link>
