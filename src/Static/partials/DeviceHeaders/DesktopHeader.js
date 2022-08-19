39b220e23081e23079d6aa75245c26193a7a4fd2
pdxgrantc <pdxgrantc@gmail.com>
2022-08-19 14:54:10 -0700

    unknown changes

--- src/Static/partials/DeviceHeaders/DesktopHeader.js
+++ src/Static/partials/DeviceHeaders/DesktopHeader.js
@@ -1,31 +1,31 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 
-export default function DesktopHeader({ header_title }) {
-    var special_nav = "About This Site"
-    var title_link = "/"
-    var special_link = "/About-This-Site"
-    if (header_title === undefined) {
-        header_title = "Grant Conklin"
-        title_link = "/"
+export default function DesktopHeader() {
+
+    if (window.location.pathname === "/") {
+        return (
+            <div class="flex justify-between">
+                <Link class="w-[35vw] bg-black pl-[5vw] text-[3.75rem] font-bold leading-[5.75rem] h-[12vh] hover:cursor-pointer mobile:text-4xl" to="/">Grant Conklin</Link>
+                <div class="pr-[5vw] flex text-[2.75rem] font-semibold mobile:text-4xl">
+                    <Link class="h-fit align-middle px-[1vw] py-[.5vw] hover:bg-button_pressed_color hover:cursor-pointer hover:ease-in-out duration-[350ms]" to="/Code">Code</Link>
+                    <Link class="h-fit align-middle px-[1vw] py-[.5vw] hover:bg-button_pressed_color hover:cursor-pointer hover:ease-in-out duration-[350ms]" to="/ToDo">Apps</Link>
+                    <Link class="h-fit align-middle px-[1vw] py-[.5vw] bg-button_accent_color hover:bg-transparent hover:cursor-pointer hover:ease-in-out duration-[350ms]" to="/about-this-site">About This Site</Link>
+                </div>
+            </div>
+        )
     }
     else {
-        special_nav = "Home"
-    }
-
-    if (header_title === "About This Site") {
-        title_link = "/About-This-Site"
-        special_link = "/"
-    }
-
-    return (
-        <div class="flex justify-between">
-            <Link class="w-[35vw] bg-black pl-[5vw] text-[3.75rem] font-bold leading-[5.75rem] h-[12vh] hover:cursor-pointer mobile:text-4xl" to={title_link}>{header_title}</Link>
-            <div class="pr-[5vw] flex text-[2.75rem] font-semibold mobile:text-4xl">
-                <Link class="h-fit align-middle px-[1vw] py-[.5vw] hover:bg-button_pressed_color hover:cursor-pointer hover:ease-in-out duration-[350ms]" to="/Code">Code</Link>
-                <Link class="h-fit align-middle px-[1vw] py-[.5vw] hover:bg-button_pressed_color hover:cursor-pointer hover:ease-in-out duration-[350ms]" to="/ToDo">Apps</Link>
-                <Link class="h-fit align-middle px-[1vw] py-[.5vw] bg-button_accent_color hover:bg-transparent hover:cursor-pointer hover:ease-in-out duration-[350ms]" to={special_link}>{special_nav}</Link>
+        return (
+            <div class="flex justify-between">
+                <Link class="w-[35vw] bg-black pl-[5vw] text-[3.75rem] font-bold leading-[5.75rem] h-[12vh] hover:cursor-pointer mobile:text-4xl" to="/">Grant Conklin</Link>
+                <div class="pr-[5vw] flex text-[2.75rem] font-semibold mobile:text-4xl">
+                    <Link class="h-fit align-middle px-[1vw] py-[.5vw] hover:bg-button_pressed_color hover:cursor-pointer hover:ease-in-out duration-[350ms]" to="/Code">Code</Link>
+                    <Link class="h-fit align-middle px-[1vw] py-[.5vw] hover:bg-button_pressed_color hover:cursor-pointer hover:ease-in-out duration-[350ms]" to="/ToDo">Apps</Link>
+                    <Link class="h-fit align-middle px-[1vw] py-[.5vw] bg-button_accent_color hover:bg-transparent hover:cursor-pointer hover:ease-in-out duration-[350ms]" to="/">Home</Link>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+
 }
