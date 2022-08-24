1049ae0a08bb2187febd21cc2a92e57140b45c22
pdxgrantc <pdxgrantc@gmail.com>
2022-08-24 13:37:04 -0700

    current item update

--- src/Pages/ShoppingList/partials/CurrentItems.js
+++ src/Pages/ShoppingList/partials/CurrentItems.js
@@ -3,24 +3,24 @@ import React from 'react'
 export default function CurrentItems() {
     return (
         <div class="bg-black w-1/2 px-[2vw] py-[2vh]">
-            <div class="flex gap-10">
+            <div class="flex justify-between gap-5">
                 <div class="flex flex-col">
                     <div class="text-3xl">
-                        <h2 class="text-3xl">Title:</h2>
+                        <h2 class="text-3xl">Name:</h2>
                     </div>
-                    <div class="text-2xl">
+                    <div class="text-xl">
                         <p>Bread</p>
                     </div>
-                    <div class="text-2xl">
+                    <div class="text-xl">
                         <p>Bread</p>
                     </div>
-                    <div class="text-2xl">
+                    <div class="text-xl">
                         <p>Bread</p>
                     </div>
-                    <div class="text-2xl">
+                    <div class="text-xl">
                         <p>Bread</p>
                     </div>
-                    <div class="text-2xl">
+                    <div class="text-xl">
                         <p>Bread</p>
                     </div>
                 </div>
@@ -98,24 +98,22 @@ export default function CurrentItems() {
                             <label for="item3">Quinn</label>
                         </div>
                     </div>
-                    <div class="text-xl w-64 flex justify-between">
-                        <div>
-                            <input type="checkbox" name="item1"></input>
-                            <label for="vehicle1">Grant</label>
-                        </div>
-                        <div>
-                            <input type="checkbox" name="item2"></input>
-                            <label for="item2">Daniel</label>
-                        </div>
-                        <div>
-                            <input type="checkbox" name="item3"></input>
-                            <label for="item3">Quinn</label>
-                        </div>
-                    </div>
                 </div>
                 <div class="flex flex-col">
                     <div class="text-3xl">
                         <h2>Price:</h2>
+                    </div>
+                    <div class="text-xl">
+                        <p>$10.00</p>
+                    </div>
+                    <div class="text-xl">
+                        <p>$10.00</p>
+                    </div>
+                    <div class="text-xl">
+                        <p>$10.00</p>
+                    </div>
+                    <div class="text-xl">
+                        <p>$10.00</p>
                     </div>
                     <div class="text-xl">
                         <p>$10.00</p>
